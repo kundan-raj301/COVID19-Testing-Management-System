62e58012134d0849af3e14cd6a12d35e4dcdbbd7
Kundan Kumar <rajkundan301@gmail.com>
2023-08-22 13:35:33 +0530

    second commit

---
+++ covid-tms/includes/footer.php
@@ -0,0 +1,9 @@
+ <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; kundan_raj301</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
